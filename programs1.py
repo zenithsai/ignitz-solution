3a4d9eb7df5e49e22a13dba35b9bf240ed8b42f7
nannu <zenith24@gmail.com>
2023-02-03 15:29:18 +0530

    secondcommited

--- programs1.py
+++ programs1.py
@@ -1,1 +1,41 @@
 https://github.com/zenithsai/ignitz-solution.git
+def calculate(num1, operator, num2):
+#     if operator == "+":
+#         return num1 + num2
+#     elif operator == "-":
+#         return num1 - num2
+#     elif operator == "*":
+#         return num1 * num2
+#     elif operator == "/":
+#         return num1 / num2
+#     else:
+#         return "Invalid operator"
+# num1 = 10
+# num2 = 5
+# operator = "+"
+# result = calculate(num1, operator, num2)
+# print(result)
+
+# num1 = 20
+# num2 = 10
+# operator = "-"
+# result = calculate(num1, operator, num2)
+# print(result)
+
+# num1 = 4
+# num2 = 2
+# operator = "*"
+# result = calculate(num1, operator, num2)
+# print(result)
+
+# num1 = 10
+# num2 = 2
+# operator = "/"
+# result = calculate(num1, operator, num2)
+# print(result)
+
+# num1 = 10
+# num2 = 5
+# operator = "^"
+# result = calculate(num1, operator, num2)
+# print(result)
